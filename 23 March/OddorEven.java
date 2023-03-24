7257131b9fcf79115001cd02460008a98363ec03
Sudarshan Balkrushan Waghmode <112853463+Sudarshan2012@users.noreply.github.com>
2023-03-24 10:15:34 +0530

    Add files via upload

---
+++ 23 March/OddorEven.java
@@ -0,0 +1,17 @@
+//java program to find Number is odd or Even
+
+class OddorEven       //creating class
+{
+    public static void main(String args[])   //main method
+	{
+	  int i=5;       
+	  if(i%2==0)    //if condition 
+	  {
+	    System.out.println("Number is Even:");
+	  }
+	  else
+	  {
+	     System.out.println("Number is Odd:");
+	  }
+    }
+}
